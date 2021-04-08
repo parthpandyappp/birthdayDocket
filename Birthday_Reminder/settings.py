f5c924459e59995c89728a8ddcaa23c78b79e555
parthpandyappp <parthpandyappp@gmail.com>
2021-04-08 19:43:46 +0530

    DEPLOY-2

--- Birthday_Reminder/settings.py
+++ Birthday_Reminder/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Birthday_Reminder.urls'
